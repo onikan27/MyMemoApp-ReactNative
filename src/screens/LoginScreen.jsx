041d796055c36f9ede60436e5059343171c49d85
onikan27 <ude0721juji@icloud.com>
2021-05-12 11:58:08 +0900

    feat: Add Login page

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -11,16 +11,16 @@ const LoginScreen = () => {
   return (
     <View style={styles.container}>
       <AppBar />
-      <View>
-        <Text>Log In</Text>
-        <TextInput value="Email Address" />
-        <TextInput value="Password" />
-        <View>
-          <Text>Submit</Text>
+      <View style={styles.inner}>
+        <Text style={styles.title}>Log In</Text>
+        <TextInput value="Email Address" style={styles.input} />
+        <TextInput value="Password" style={styles.input} />
+        <View style={styles.buttonContainer}>
+          <Text style={styles.buttonLabel}>Submit</Text>
         </View>
-        <View>
-          <Text>Not registered?</Text>
-          <Text>Sign up here!</Text>
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>Not registered?</Text>
+          <Text style={styles.footerLink}>Sign up here!</Text>
         </View>
       </View>
     </View>
@@ -29,6 +29,53 @@ const LoginScreen = () => {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    backgroundColor: '#F0F4F8',
+  },
+  inner: {
+    paddingHorizontal: 27,
+    paddingVertical: 24,
+  },
+  title: {
+    fontSize: 24,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    marginBottom: 24,
+  },
+  input: {
+    fontSize: 16,
+    height: 48,
+    borderColor: '#DDDDDD',
+    borderWidth: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal: 8,
+    marginBottom: 16,
+  },
+  buttonContainer: {
+    backgroundColor: '#467FD3',
+    borderRadius: 4,
+    alignSelf: 'flex-start',
+    marginBottom: 24,
+  },
+  buttonLabel: {
+    fontSize: 16,
+    lineHeight: 32,
+    paddingHorizontal: 32,
+    paddingVertical: 8,
+    color: '#fff',
+  },
+  footer: {
+    flexDirection: 'row',
+  },
+  footerText: {
+    fontSize: 14,
+    lineHeight: 24,
+    marginRight: 8,
+  },
+  footerLink: {
+    fontSize: 14,
+    lineHeight: 24,
+    color: '#467FD3',
   },
 });
 
